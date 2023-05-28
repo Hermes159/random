2998d1e2a7d25ac332b835b39e4f2bba078ab8bf
Gui <130260394+Hermes159@users.noreply.github.com>
2023-05-28 14:18:10 +0100

    wifipassspamer.py
    Just a wi-fi pass spammer for windows that i've tried to code(doesnt work in windows cmd) to help a friend trying to get the neighbours wi-fi password

--- wifipassspamer.py
+++ wifipassspamer.py
@@ -1,11 +1,8 @@
-#from random import choice as cc
 from pynput.keyboard import Key, Controller
 keyboard = Controller()
-#f = keyboard.press(Key.enter)
-#fs = keyboard.release(Key.enter)
 list = ["pass2","pass1","pass3"]
 
-##def crack():
+
 wifi = input("Nome do wifi nabo!: ")
 for i in list:
     print(f"netsh wlan set hostednetwork mode=allow SSID=({wifi}) key=({i})")
@@ -26,7 +23,7 @@ for i in list:
 
 
 
-#crack()  
+ 
 
 
     
